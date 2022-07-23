1e82bc04191a83936494d947361d44eec3c1b94c
Purevsuren Byambasuren <purevsurenby@gmail.com>
2022-07-23 15:34:14 +0200

    initial

--- token-sale/src/lib.rs
+++ token-sale/src/lib.rs
@@ -8,26 +8,46 @@ blueprint! {
     }
 
     impl TokenSales {
-        pub fn new(price_per_token: Decimal) -> ComponentAddress {
+        pub fn new(price_per_token: Decimal) -> (ComponentAddress, Bucket) {
             let bucket: Bucket = ResourceBuilder::new_fungible()
-            .metadata("name", "Useful Token")
-            .metadata("symbol", "USEFUL")
-            .initial_supply(1_000);
+            .metadata("name", "Scrypto Challenge Token")
+            .metadata("symbol", "SCT")
+            .metadata("team-member-1-ticket-number", "4054057019")
+            .metadata("team-member-2-ticket-number", "4073361989")
+            .metadata("team-member-3-ticket-number", "4066471649")
+            .metadata("team-member-4-ticket-number", "4102398759")
+            .divisibility(DIVISIBILITY_MAXIMUM)
+            .initial_supply(100_000);
 
-            Self {
+            let seller_badge = ResourceBuilder::new_fungible()
+            .metadata("name", "Seller Badge")
+            .divisibility(DIVISIBILITY_NONE)
+            .initial_supply(1);
+
+            let component = Self {
                 useful_tokens_vault: Vault::with_bucket(bucket),
-                xrd_tokens_vault: Vault:: Vault::new(RADIX_TOKEN),
+                xrd_tokens_vault: Vault::new(RADIX_TOKEN),
                 price_per_token: price_per_token 
             }
-            .instatiate()
-            .globalize()
+            .instantiate()
+            .globalize();
+
+            (component, seller_badge)
         }
 
         pub fn buy(&mut self, funds: Bucket) -> Bucket {
             let purchase_amount: Decimal = funds.amount() / self.price_per_token;
             self.xrd_tokens_vault.put(funds);
-            self.useful_tokens_vault.take(purchase_amount);
+            self.useful_tokens_vault.take(purchase_amount)
         }
 
+        // pub fn withdraw_funds(&mut self, amount: Decimal) -> Bucket {
+        //     // Your `withdraw_funds` implementation goes here.
+        // }
+  
+        // pub fn change_price(&mut self, price: Decimal) {
+        //     // Your `change_price` implementation goes here.
+        // }
+ 
     }
 }
