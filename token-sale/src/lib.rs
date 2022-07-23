7bb387868853013bf949c409609b85440d3ba0bf
Kokufuu <PEjk59h0>
2022-07-23 16:27:35 +0200

    finished

--- token-sale/src/lib.rs
+++ token-sale/src/lib.rs
@@ -5,7 +5,6 @@ blueprint! {
         useful_tokens_vault: Vault,
         xrd_tokens_vault: Vault,
         price_per_token: Decimal,
-        seller_badge: ResourceDef,
     }
 
     impl TokenSales {
@@ -25,13 +24,18 @@ blueprint! {
             .divisibility(DIVISIBILITY_NONE)
             .initial_supply(1);
 
+            let access_rules: AccessRules = AccessRules::new()
+            .method("withdraw_funds", rule!(require(seller_badge.resource_address())))
+            .method("change_price", rule!(require(seller_badge.resource_address())))
+            .default(rule!(allow_all));
+
             let component = Self {
                 useful_tokens_vault: Vault::with_bucket(bucket),
                 xrd_tokens_vault: Vault::new(RADIX_TOKEN),
                 price_per_token: price_per_token,
-                seller_badge: seller_badge.resource_def(),
             }
             .instantiate()
+            .add_access_check(access_rules)
             .globalize();
 
             (component, seller_badge)
@@ -43,11 +47,10 @@ blueprint! {
             self.useful_tokens_vault.take(purchase_amount)
         }
 
-        // pub fn withdraw_funds(&mut self, amount: Decimal) -> Bucket {
-        //     // Your `withdraw_funds` implementation goes here.
-        // }
+        pub fn withdraw_funds(&mut self, amount: Decimal) -> Bucket {
+            self.xrd_tokens_vault.take(amount)
+        }
 
-        #[auth(seller_badge)]
         pub fn change_price(&mut self, price: Decimal) {
             self.price_per_token = price
         }
